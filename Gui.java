0c137c7be7d34786ae79686b6a79f821a130ab9a
Mattis Niemann <mattisniemann08@gmail.com>
2026-05-09 22:28:56 +0200

    ..

--- Gui.java
+++ Gui.java
@@ -5,21 +5,30 @@ import javax.swing.JLabel;
 
 public class Gui {
 
-    private static JLabel getCardLabel() {
-        // Ensure the path is correct and relative to the classpath
-        ImageIcon cardIcon = new ImageIcon("cards/7-C.png");
-        // Scale the image to make it smaller
+    private static JLabel getCardLabel(Card card) {
+        // Der Pfad wird aus der Farbe und dem Wert der Karte generiert
+        // Beispiel: "cards/" + "Herz" + "-" + "Ass" + ".png"
+        String path = "cards/" + card.getColor() + "-" + card.getValue() + ".png";
+        ImageIcon cardIcon = new ImageIcon(path);
+
+        // Falls das Bild nicht gefunden wurde (Fehlerschutz)
+        if (cardIcon.getIconWidth() == -1) {
+            System.out.println("Bild nicht gefunden: " + path);
+            return new JLabel("Bild fehlt"); 
+        }
+    
         Image scaledImage = cardIcon.getImage().getScaledInstance(100, 150, Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon = new ImageIcon(scaledImage);
-        return new JLabel(scaledIcon);
+        return new JLabel(new ImageIcon(scaledImage));
     }
+
+
 
     public static void main(String[] args) {
         // Example usage of getCardLabel
         JFrame frame = new JFrame("Card Display");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 450);
-        JLabel cardLabel = getCardLabel();
+        JLabel cardLabel = getCardLabel(Main.playerHand.get(0)); // Beispiel: die erste Karte der Spielerhand anzeigen
         frame.add(cardLabel);
         frame.setVisible(true);
         System.out.println("Card label created: " + cardLabel);
--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
 
         startGame(); // spiel startet
+        Gui.main(args); // GUI wird gestartet
 
     }
 
